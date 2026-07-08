d86f77037226a702c5356db6252a8b5cd91e99e0
Nav <nav@navnav.me>
2026-07-08 19:55:35 +0100

    Removed unnecessary setting of config index for power debugger tool

--- src/DebugToolDrivers/Microchip/PowerDebugger/PowerDebugger.cpp
+++ src/DebugToolDrivers/Microchip/PowerDebugger/PowerDebugger.cpp
@@ -8,8 +8,7 @@ namespace DebugToolDrivers::Microchip
             PowerDebugger::USB_VENDOR_ID,
             PowerDebugger::USB_PRODUCT_ID,
             PowerDebugger::CMSIS_HID_INTERFACE_NUMBER,
-            false,
-            PowerDebugger::USB_CONFIGURATION_INDEX
+            false
         )
     {}
 }
--- src/DebugToolDrivers/Microchip/PowerDebugger/PowerDebugger.hpp
+++ src/DebugToolDrivers/Microchip/PowerDebugger/PowerDebugger.hpp
@@ -12,7 +12,6 @@ namespace DebugToolDrivers::Microchip
     public:
         static constexpr std::uint16_t USB_VENDOR_ID = 0x03eb;
         static constexpr std::uint16_t USB_PRODUCT_ID = 0x2144;
-        static constexpr std::uint8_t USB_CONFIGURATION_INDEX = 0;
         static constexpr std::uint8_t CMSIS_HID_INTERFACE_NUMBER = 0;
 
         explicit PowerDebugger(const DebugToolConfig& debugToolConfig);
